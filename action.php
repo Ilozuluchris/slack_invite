96f12a6d0b6cf2650a05fb7357571541d7ba44a9
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-13 16:51:46 +0100

    perfected the curl command

--- action.php
+++ action.php
@@ -1,7 +1,12 @@
 <?php
-        $email = $_POST['email'];
+        $email = "jon@gmail.com"; #$_POST['email'];
         $team_url = "academia-ng.slack.com";
+        $token = "insert_token";
 		$real_url = 'https://' . $team_url .'/api/users.admin.invite';
 		$send = 'email='.$email.'&token='.$token.'&set_active=true';
         #Todo pass in command line arguments and use that for your page
         #todo put php and html code on the same page.
+        $command = "curl -X POST".$real_url. "\
+        --data" .$send."\
+        --compressed";
+        $output = shell_exec( $command);

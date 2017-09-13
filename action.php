0019d8be0c8d91f4c5085e1f3e269e8d5100fe26
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-13 22:09:35 +0100

    Enabled working with command line arguments

--- action.php
+++ action.php
@@ -6,7 +6,20 @@
 		$send = 'email='.$email.'&token='.$token.'&set_active=true';
         #Todo pass in command line arguments and use that for your page
         #todo put php and html code on the same page.
+        if ($argc == 2)
+        {
+            echo  "passed in args";
+            $token = $argv[1]; #for  security purposes argument is passed in via command line.
+        }
+
+        else
+        {
+            echo die("Pass in the right number of arguments");
+
+
+        }
         $command = "curl -X POST".$real_url. "\
         --data" .$send."\
         --compressed";
-        $output = shell_exec( $command);
+        echo $command;
+        #$output = shell_exec( $command);

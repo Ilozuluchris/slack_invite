fff8cde8ada7e9efb986430ceb200990475a0cbb
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-13 16:30:39 +0100

    Some more corrections

--- action.php
+++ action.php
@@ -1,6 +1,5 @@
 <?php
         $email = $_POST['email'];
-        $token =  "xoxp-150107414595-150872153463-240627841767-6cbf2ec522dc9842531642cb979b8b1b";
         $team_url = "academia-ng.slack.com";
 		$real_url = 'https://' . $team_url .'/api/users.admin.invite';
 		$send = 'email='.$email.'&token='.$token.'&set_active=true';

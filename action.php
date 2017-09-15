a9c9f6f049cec665915c9e80141c4523aacac273
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-15 23:54:50 +0100

    Made form work with php code

--- action.php
+++ action.php
@@ -1,26 +1,15 @@
 <?php
         #todo: make php output index php then make it run from command line argument
-        $email = "jon@gmail.com"; #$_POST['email'];
+        $email = $_POST["email"];
         $team_url = "academia-ng.slack.com"; #put in slack team url like myslackteam.slack.com
-        $token = "insert_token"; #put in your token code
+        $token = "40044223985-701b34872ac74e7cc7e2f358deec9238"; #put in your token code
 		$real_url = 'https://' . $team_url .'/api/users.admin.invite';
 		$send = 'email='.$email.'&token='.$token.'&set_active=true';
         #Todo pass in command line arguments and use that for your page
         #todo put php and html code on the same page.
-        if ($argc == 2)
-        {
-            echo  "passed in args";
-            $token = $argv[1]; #for  security purposes argument is passed in via command line.
-        }
-
-        else
-        {
-            echo die("Pass in the right number of arguments");
-
-
-        }
         $command = "curl -X POST".$real_url. "\
         --data" .$send."\
         --compressed";
         echo $command;
         #$output = shell_exec( $command);#this sends the api call
+        #echo $output;

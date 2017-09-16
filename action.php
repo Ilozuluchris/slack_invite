995297147282a1e975ad50ee2878aba62f06b6ba
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-16 23:55:09 +0100

    Got curl command to work

--- action.php
+++ action.php
@@ -1,8 +1,8 @@
 <?php
         #todo: make php output index php then make it run from command line argument
-        $email = "iloz@gmail.com";#$_GET["email"];
+        $email = "tessy@dispostable.com";#$_GET["email"];
         $team_url = "academia-ng.slack.com"; #put in slack team url like myslackteam.slack.com
-        $token = "40044223985-701b34872ac74e7cc7e2f358deec9238"; #put in your token code
+        $token = "xoxp-150107414595-150872153463-240044223985-701b34872ac74e7cc7e2f358deec9238"; #put in your token code
 		$real_url = "'".'https://' . $team_url .'/api/users.admin.invite'."'";
 		$send = "'".'email='.$email.'&token='.$token.'&set_active=true'."'";
         #Todo pass in command line arguments and use that for your page

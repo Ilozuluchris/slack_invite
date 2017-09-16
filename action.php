fd0035c1c70fe0d14141f1c3cbfe5d51b674a20b
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-17 00:33:29 +0100

    Added better error handling

--- action.php
+++ action.php
@@ -1,21 +1,34 @@
 <?php
-        #todo: make php output index php then make it run from command line argument
-        $email = "tessy@dispostable.com";#$_GET["email"];
+#Todo find out why token is been revoked and how to use normal token(not legacy token)
+#Todo Get good background images
+#Todo figure out how to read setting from settings.json file so uses do not need to change anything in the code
+        $email = "yomi@dispostable.com";#$_GET["email"];
+
         $team_url = "academia-ng.slack.com"; #put in slack team url like myslackteam.slack.com
         $token = "xoxp-150107414595-150872153463-240044223985-701b34872ac74e7cc7e2f358deec9238"; #put in your token code
 		$real_url = "'".'https://' . $team_url .'/api/users.admin.invite'."'";
 		$send = "'".'email='.$email.'&token='.$token.'&set_active=true'."'";
-        #Todo pass in command line arguments and use that for your page
-        #todo put php and html code on the same page.
+
         $command = "curl -X POST ".$real_url." \
         --data " .$send ." \
         --compressed";
 
-/*
-curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
---data 'email=EMAIL&token=TOKEN&set_active=true' \
---compressed
-*/
-        echo $command;
+
         $output = exec( $command);#this sends the api call
         echo $output;
+
+        if (strpos($output, 'true') !== false) {
+            echo "An invite link has been sent to you";
+        }
+
+        elseif (strpos($output, 'already_invited') !== false) {
+            echo "You have already been invited.";
+        }
+
+        elseif (strpos($output, "token_revoked") !== false) {
+            echo "The token used for authentication has been revoked,Contact the admin of the slack team";
+        }
+        else{
+            echo "Some thing went wrong";
+        }
+
